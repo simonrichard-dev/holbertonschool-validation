eed1da072b290c3fb71542020764a17d944e33d7
simonrichard-dev <simon.insa@gmail.com>
2023-05-10 16:15:47 +0200

    fix main_test.go because missing ","

--- module2_task2/main_test.go
+++ module2_task2/main_test.go
@@ -42,7 +42,7 @@ func Test_HelloHandler(t *testing.T) {
 		queryString: "name=",
 		responseCode: 400,
 		body: "",
-	}
+	},
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
